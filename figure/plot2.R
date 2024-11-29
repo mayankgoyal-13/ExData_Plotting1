034d1dd5d9ca621aab333dcdddfb1f02f9877cd2
Mayank Goyal <65842833+mayankgoyal-13@users.noreply.github.com>
2024-11-29 22:10:15 +0530

    Add files via upload

---
+++ figure/plot2.R
@@ -0,0 +1,23 @@
+library("data.table")
+
+# Reads data from file
+powerDT <- data.table::fread(input = "household_power_consumption.txt"
+                             , na.strings="?"
+)
+
+# prevents Scientific Notation
+powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+
+powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+
+# filter Dates for 2007-02-01 and 2007-02-02
+powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+
+png("plot2.png", width=480, height=480)
+
+# Plot 2
+plot(x = powerDT[, dateTime]
+     , y = powerDT[, Global_active_power]
+     , type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+dev.off()
